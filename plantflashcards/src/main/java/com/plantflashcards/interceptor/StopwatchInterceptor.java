f41d7a08629ec11530ac239d3767b7ec65656462
jonesbr <brandan.jones@uc.edu>
2018-02-21 22:26:06 -0500

    Factory Method

---
+++ plantflashcards/src/main/java/com/plantflashcards/interceptor/StopwatchInterceptor.java
@@ -0,0 +1,38 @@
+package com.plantflashcards.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Log time in and out of web requests to our application.
+ * @author ucint
+ *
+ */
+@Component("stopwatchInterceptor")
+public class StopwatchInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

c08f6c9dddac0231f7545b1da32f297d5d890d7f
jonesbr <brandan.jones@uc.edu>
2018-03-13 19:07:00 -0400

    Select the attributes we will use to decorate our plants.

--- plantflashcards/src/main/java/com/plantflashcards/ui/HomePageController.java
+++ plantflashcards/src/main/java/com/plantflashcards/ui/HomePageController.java
@@ -1,7 +1,9 @@
 package com.plantflashcards.ui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +13,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomePageController {
 	
+	// all attributes that are available for selection.
+	Map<String, PlantDecorator>  allPlantDecorators;
+	
 	// The decorators that apply for our plants.
 	private List<PlantDecorator> plantDecorators;
 	
 	public HomePageController() {
 		plantDecorators = new ArrayList<PlantDecorator>();
+		allPlantDecorators = new HashMap<String, PlantDecorator>();
+		
+		// instantiate each of our decorators and add them.
+		// TODO: move this to a Spring configuration file.
+		Woody woody = new Woody();
+		Herbaceous herbaceous = new Herbaceous();
+		Native nativeGeo = new Native();
+		
+		allPlantDecorators.put(woody.getLabel(), woody);
+		allPlantDecorators.put(herbaceous.getLabel(), herbaceous);
+		allPlantDecorators.put(nativeGeo.getLabel(), nativeGeo);
+		
+		
 	}
 
 	@RequestMapping("/home")
 	public String home(@RequestParam(value="name", required=false, defaultValue="Friend") String name, Model model) {
 		model.addAttribute("name", name);
 		return "home";
+	}
+	
+	@RequestMapping("/selectattributes")
+	public String selectAttributes(Model model) {
+		// pass all possible decorators to our form.
+		model.addAttribute("components", allPlantDecorators.keySet());
+		return "selectattributes";
+	}
+	
+	@RequestMapping("/addplant")
+	public String addPlant(@RequestParam Map<String,String> requestParams) {
+		
+		return "addplant";
 	}
 	
 }

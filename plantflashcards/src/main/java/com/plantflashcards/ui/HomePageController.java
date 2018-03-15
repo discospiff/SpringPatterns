31227f0f0f132cdda7b04b69e67d6fe2f24997fc
jonesbr <brandan.jones@uc.edu>
2018-03-15 19:25:17 -0400

    5.2: Spring Annotations

--- plantflashcards/src/main/java/com/plantflashcards/ui/HomePageController.java
+++ plantflashcards/src/main/java/com/plantflashcards/ui/HomePageController.java
@@ -6,35 +6,25 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@ImportResource({"/WEB-INF/classes/applicationContext.xml"})
 public class HomePageController {
 	
-	// all attributes that are available for selection.
-	private Map<String, PlantDecorator>  allPlantDecorators;
+	@Autowired
+	private PlantAttributeGroups plantAttributeGroups;
 	
 	// The decorators that apply for our plants.
 	private List<PlantDecorator> plantDecorators;
 	
 	public HomePageController() {
 		plantDecorators = new ArrayList<PlantDecorator>();
-		allPlantDecorators = new HashMap<String, PlantDecorator>();
-		
-		// instantiate each of our decorators and add them.
-		// TODO: move this to a Spring configuration file.
-		Woody woody = new Woody();
-		Herbaceous herbaceous = new Herbaceous();
-		Native nativeGeo = new Native();
-		
-		allPlantDecorators.put(woody.getLabel(), woody);
-		allPlantDecorators.put(herbaceous.getLabel(), herbaceous);
-		allPlantDecorators.put(nativeGeo.getLabel(), nativeGeo);
-		
-		
 	}
 
 	@RequestMapping("/home")
@@ -46,7 +36,7 @@ public class HomePageController {
 	@RequestMapping("/selectattributes")
 	public String selectAttributes(Model model) {
 		// pass all possible decorators to our form.
-		model.addAttribute("components", allPlantDecorators.keySet());
+		model.addAttribute("components", plantAttributeGroups.getDecorators().keySet());
 		return "selectattributes";
 	}
 	
@@ -60,7 +50,7 @@ public class HomePageController {
 		
 		// iterate over the selected keys, and get the template identifier.
 		for (String key : selectedKeys) {
-			PlantDecorator plantDecorator = allPlantDecorators.get(key);
+			PlantDecorator plantDecorator = plantAttributeGroups.getDecorators().get(key);
 
 			if (plantDecorator != null) {
 				// a collection containing ONLY the selected decorators.
